8740af90d90e1a3f851e26824a239c8c4b6050d8
pirate2580 <90294883+pirate2580@users.noreply.github.com>
2024-11-08 00:22:57 -0500

    implement user methods

--- src/main/java/java/entity/User/CommonUser.java
+++ src/main/java/java/entity/User/CommonUser.java
@@ -1,6 +1,6 @@
 package main.java.java.entity.User;
+import main.java.java.entity.Event.Event;
 
-import main.java.java.entity.Event.CommonEvent;
 import java.util.List;
 // TODO: is this clean architecture for entities to rely on another?
 
@@ -10,8 +10,11 @@ import java.util.List;
 public class CommonUser implements User{
     private final String username;
     private final String password;
-    private List<CommonEvent> rsvpedEvents;
-    private List<CommonEvent> createdEvents;
+
+    // notice this way of setting up for a general Event interface is better than
+    // using a specific implementation
+    private List<Event> rsvpedEvents;
+    private List<Event> createdEvents;
 
     // Note that the two list attributes are not put in the constructor
 
@@ -21,6 +24,58 @@ public class CommonUser implements User{
     }
 
     @Override
-    pu
+    public String getUsername() {
+        return this.username;
+    }
 
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public void addRsvpedEvent(Event event) {
+        // notice we use dependency injection design pattern here
+        this.rsvpedEvents.add(event);
+    }
+
+    @Override
+    public void removeRsvpedEvent(Event event) {
+        // TODO: might need a special event.equals method
+        // TODO: check edge cases on implementation
+        for (int i = 0; i < this.rsvpedEvents.size(); i++){
+            if (this.rsvpedEvents.get(i).equals(event)){
+                this.rsvpedEvents.remove(i);
+                break;
+            }
+        }
+
+    }
+
+    @Override
+    public void addCreatedEvent(Event event) {
+        this.createdEvents.add(event);
+    }
+
+    @Override
+    public void removeCreatedEvent(Event event) {
+        // TODO: might need a special event.equals method
+        // TODO: check edge cases on implementation
+        for (int i = 0; i < this.createdEvents.size(); i++){
+            if (this.createdEvents.get(i).equals(event)){
+                this.createdEvents.remove(i);
+                break;
+            }
+        }
+    }
+
+    @Override
+    public List<Event> getRsvpedEvents() {
+        return this.rsvpedEvents;
+    }
+
+    @Override
+    public List<Event> getCreatedEvents() {
+        return this.createdEvents;
+    }
 }
