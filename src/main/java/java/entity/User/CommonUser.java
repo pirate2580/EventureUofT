95ac322b7cb436f22a07fe843b02f2beac77b2b3
pirate2580 <90294883+pirate2580@users.noreply.github.com>
2024-11-08 00:21:58 -0500

    add and implement user methods

--- src/main/java/java/entity/User/CommonUser.java
+++ src/main/java/java/entity/User/CommonUser.java
@@ -1,11 +1,26 @@
 package main.java.java.entity.User;
 
+import main.java.java.entity.Event.CommonEvent;
+import java.util.List;
+// TODO: is this clean architecture for entities to rely on another?
+
 /**
  * Implementation of the User interface
  */
 public class CommonUser implements User{
     private final String username;
     private final String password;
-    private List<Event> rsvpedEvents;
+    private List<CommonEvent> rsvpedEvents;
+    private List<CommonEvent> createdEvents;
+
+    // Note that the two list attributes are not put in the constructor
+
+    public CommonUser(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    pu
 
 }
