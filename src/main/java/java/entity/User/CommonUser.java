1ae60829982d762a2a0985b0268fbfada293a459
pirate2580 <90294883+pirate2580@users.noreply.github.com>
2024-11-07 23:57:32 -0500

    add boilerplate for entities event and user

---
+++ src/main/java/java/entity/User/CommonUser.java
@@ -0,0 +1,11 @@
+package main.java.java.entity.User;
+
+/**
+ * Implementation of the User interface
+ */
+public class CommonUser implements User{
+    private final String username;
+    private final String password;
+    private List<Event> rsvpedEvents;
+
+}
