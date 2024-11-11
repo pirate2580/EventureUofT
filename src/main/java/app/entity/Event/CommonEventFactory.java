373d9b53d66a00a6d4c44599ea89bd955b8a9a91
Andrew Sasmito <98786007+AndrewSasmito@users.noreply.github.com>
2024-11-11 16:06:40 -0500

    Update CommonEventFactory.java
    Added functionality

--- src/main/java/app/entity/Event/CommonEventFactory.java
+++ src/main/java/app/entity/Event/CommonEventFactory.java
@@ -1,4 +1,14 @@
-package app.entity.Event;
+package main.java.java.entity.Event;
 
-public class CommonEventFactory {
+import java.util.List;
+
+/**
+ * Factory for creating CommonEvents
+ */
+public class CommonEventFactory implements EventFactory {
+    public Event create(String eventId, String organizer, String title, String description, String dateTime,
+                        int capacity, int latitude, int longitude, List<String> tags) {
+        return new CommonEvent (eventId, organizer, title, description, dateTime,
+        capacity, latitude, longitude, tags);
+    }
 }
