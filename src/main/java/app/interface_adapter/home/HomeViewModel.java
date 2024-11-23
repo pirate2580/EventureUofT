0ca00b2f5c56e822663eb2a172826942e5c3fc36
FionaV2400 <139073560+FionaVerzivolli@users.noreply.github.com>
2024-11-23 01:25:58 -0500

    Added TO-DOs and comments

--- src/main/java/app/interface_adapter/home/HomeViewModel.java
+++ src/main/java/app/interface_adapter/home/HomeViewModel.java
@@ -1,7 +1,7 @@
 package app.interface_adapter.home;
 
 import app.interface_adapter.ViewModel;
-
+//TODO: Figure out what to put here.
 public class HomeViewModel extends ViewModel<HomeState> {
     public HomeViewModel() {
         super("Home");
