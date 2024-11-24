b061abc1a86c1f24b494304ad62b0104e9068f6e
pirate2580 <90294883+pirate2580@users.noreply.github.com>
2024-11-23 21:54:05 -0500

    rename LoginUserDataaccessInterface for consistency

---
+++ src/main/java/app/use_case/login/LoginUserDataAccessInterface.java
@@ -0,0 +1,15 @@
+package app.use_case.login;
+
+import app.entity.User.User;
+
+/**
+ * Data Access Object for the Login Use Case.
+ */
+public interface LoginUserDataAccessInterface {
+    /**
+     * Finds a user by the username.
+     * @param username The username to search for.
+     * @return The User object if found, otherwise null.
+     */
+    User findUserByUsername(String username);
+}
