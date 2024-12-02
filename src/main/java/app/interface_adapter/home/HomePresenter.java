32166c5d1ae37dbf70cb3df9416741bd4d8ac673
FionaV2400 <139073560+FionaVerzivolli@users.noreply.github.com>
2024-12-02 18:15:53 -0500

    added documentation and removed redundant code

--- src/main/java/app/interface_adapter/home/HomePresenter.java
+++ src/main/java/app/interface_adapter/home/HomePresenter.java
@@ -20,15 +20,10 @@ import app.view.ViewRSVPView;
  * Take output data and turn it into raw strings
  * or numbers to be displayed. Can be used to store information
  * needed for map markers, such as event names, descriptions, etc.
- * must send it to the view model (??) not sure, according to
- * clean architecture diagram it does that, but imo it would
- * make more sense to send it to the home view.
+ * must send it to the view model
  * */
 public class HomePresenter implements HomeOutputBoundary {
-    //TODO: Take output data and turn it into raw strings to store info
-
-    //TODO: Send info to view model(?)
-
+    // initialize all variables
     private final ViewManagerModel viewManagerModel;
     private final LoginViewModel loginViewModel;
     private final CreateEventViewModel createEventViewModel;
@@ -38,6 +33,7 @@ public class HomePresenter implements HomeOutputBoundary {
 
     private final ModifyEventViewModel modifyEventViewModel;
 
+    // set values
     public HomePresenter(ViewManagerModel viewManagerModel, LoginViewModel loginViewModel, CreateEventViewModel createEventViewModel, FilterEventViewModel filterEventViewModel, ViewRSVPViewModel viewRSVPViewModel, ViewCreatedEventsViewModel viewCreatedEventsViewModel
             , ModifyEventViewModel modifyEventViewModel) {
         this.viewManagerModel = viewManagerModel;
@@ -49,22 +45,33 @@ public class HomePresenter implements HomeOutputBoundary {
         this.modifyEventViewModel = modifyEventViewModel;
     }
 
+    /**
+     * Function to switch from home view to create event view.
+     * */
     @Override
     public void switchToCreateEventView() {
+        // Get states
         final CreateEventState createEventState = createEventViewModel.getState();
         final LoginState loginState = loginViewModel.getState();
         createEventState.setUsernameState(loginState.getUsername());
+        // Track the property
         this.createEventViewModel.firePropertyChanged();
         viewManagerModel.setState(createEventViewModel.getViewName());
         viewManagerModel.firePropertyChanged();
     }
 
+    /**
+     * Function to switch from home view to log in view.
+     * */
     @Override
     public void switchToLoginView() {
         viewManagerModel.setState(loginViewModel.getViewName());
         viewManagerModel.firePropertyChanged();
     }
 
+    /**
+     * Function to switch from home view to filter event view.
+     * */
     @Override
     public void switchToFilterEventView() {
         final FilterEventState filterEventState = filterEventViewModel.getState();
@@ -72,10 +79,12 @@ public class HomePresenter implements HomeOutputBoundary {
         filterEventState.setUsernameState(loginState.getUsername());
         this.filterEventViewModel.firePropertyChanged();
         viewManagerModel.setState(filterEventViewModel.getViewName());
-//        System.out.println(filterEventViewModel.getViewName());
         viewManagerModel.firePropertyChanged();
     }
 
+    /**
+     * Function to switch from home view to RSVP view.
+     * */
     @Override
     public void switchToViewRSVPView() {
         final ViewRSVPState viewRSVPState = viewRSVPViewModel.getState();
@@ -86,6 +95,9 @@ public class HomePresenter implements HomeOutputBoundary {
         viewManagerModel.firePropertyChanged();
     }
 
+    /**
+     * Function to switch from home view to view created events view.
+     * */
     @Override
     public void switchToViewCreatedEventsView() {
         final ViewCreatedEventsState viewCreatedEventsState = viewCreatedEventsViewModel.getState();
@@ -97,6 +109,9 @@ public class HomePresenter implements HomeOutputBoundary {
         viewManagerModel.firePropertyChanged();
     }
 
+    /**
+     * Function to switch from home view to modify event view.
+     * */
     @Override
     public void switchToModifyEventView() {
         final ModifyEventState modifyEventState = modifyEventViewModel.getState();
